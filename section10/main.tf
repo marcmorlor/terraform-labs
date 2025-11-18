4f57b39a1ba717fc84d28e3995d572b4ab002abb
NakkoWorld <marcmorlor@gmail.com>
2025-11-18 17:43:04 +0100

    Section 10: Terraform init

--- section10/main.tf
+++ section10/main.tf
@@ -1,3 +1,7 @@
 resource "random_string" "random" {
   length = 10
 }
+module "s3-bucket_example_complete" {
+  source  = "terraform-aws-modules/s3-bucket/aws//examples/complete"
+  version = "2.10.0"
+}
