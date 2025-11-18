ef6b7f4a7d4ff39a5768e7ba4491a61f659d2755
NakkoWorld <marcmorlor@gmail.com>
2025-11-18 16:42:59 +0100

    Section 9: Terraform module scope

--- section9/main.tf
+++ section9/main.tf
@@ -358,3 +358,31 @@ output "public_dns" {
 output "size" {
   value = module.server.size
 }
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = ">= 9.0.0"
+ 
+  # Autoscaling group
+  name = "myasg"
+ 
+  vpc_zone_identifier = [aws_subnet.private_subnets["private_subnet_1"].id, 
+  aws_subnet.private_subnets["private_subnet_2"].id, 
+  aws_subnet.private_subnets["private_subnet_3"].id]
+  min_size            = 0
+  max_size            = 1
+  desired_capacity    = 1
+ 
+  # Launch template
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+ 
+  tags = {
+    Name = "Web EC2 Server 2"
+  }
+ 
+}
+
+output "asg_group_size" {
+  value = module.autoscaling.autoscaling_group_max_size
+}
