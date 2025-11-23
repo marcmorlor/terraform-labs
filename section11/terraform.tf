31171667ecbcf93935c132d4fb497cc4a5c770a9
NakkoWorld <marcmorlor@gmail.com>
2025-11-23 21:07:39 +0100

    Section 11: Terraform remote state enchanced backend

--- section11/terraform.tf
+++ section11/terraform.tf
@@ -3,11 +3,12 @@ terraform {
   required_providers {
 
   }
-  backend "http" {
-    address        = "http://localhost:5001/terraform_state/my_state"
-    lock_address   = "http://localhost:5001/terraform_lock/my_state"
-    lock_method    = "PUT"
-    unlock_address = "http://localhost:5001/terraform_lock/my_state"
-    unlock_method  = "DELETE"
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "PizzaPesto"
+
+    workspaces {
+      name = "my-aws-app"
+    }
   }
 }
